27d5e8ef2467efc4f4ea152549e492fcabe3a024
julesgtz <113105305+julesgtz@users.noreply.github.com>
2023-05-04 10:52:47 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ mens=(mont*tauxmens*((1+tauxmens)**(12*annee)))/(((1+tauxmens)**(12*annee))-1)
 montint=(mens*annee*12)-20000
 mens=round(mens,2)
 montint=round(montint,2)
+
 print("La mensulaité avec intérêts est de :", mens)
 print("Le montant des intérêts remboursés sont de :",montint)
 print("Le taux mensuel est de ",tauxmens)
 print("Tableau d'amortissement : ")
 print("Mois - Mensualité - Intérêts - Capital rembousé - Capital restant du - Intérêts remboursés")
-for i in range(0,24):
-    print(i," - ", mens," - ",)
+capitalrest = 20000
+interetsremb=0
+for i in range(1,25):
+    interets=capitalrest*tauxmens
+    capitalrembourse=mens-interets
+    capitalrest=capitalrest-capitalrembourse
+    interetsremb=interetsremb+interets
+    print(i," - ", round(mens,1)," - ", round(interets,1)," - ", round(capitalrembourse,1)," - ", round(capitalrest,1) ," - ", round(interetsremb,2))
